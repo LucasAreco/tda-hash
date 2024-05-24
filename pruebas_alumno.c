efb2b8016e46c0968fe6de7077029e629f37ff32
LucasAreco <lareco@fi.uba.ar>
2024-05-23 23:24:30 -0300

    Primeras funciones y pruebas de creación.

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -1,17 +1,28 @@
 #include "pa2m.h"
+#include "src/hash.h"
 
-void prueba_simple()
+#define CAPACIDAD_VALIDA 5
+
+void creacion_de_hash() 
 {
-	int i = 14;
-	int j = 16;
-	pa2m_afirmar(i == j, "i=14 es diferente de j=16");
+	hash_t* nuevo_hash = hash_crear(CAPACIDAD_VALIDA);
+
+	pa2m_afirmar(nuevo_hash != NULL, "Un hash válido se crea correctamente");
+	pa2m_afirmar(hash_cantidad(nuevo_hash) == 0, "Un hash vacío no tiene elementos.");
+
+	hash_destruir(nuevo_hash);
+
+	hash_t* otro_hash = hash_crear(0);
+	pa2m_afirmar(otro_hash != NULL, "Al pasar capacidad 0, el hash se crea correctemente.");
+
+
+	hash_destruir(otro_hash);
 }
 
 int main()
 {
-	pa2m_nuevo_grupo(
-		"\n======================== XXX ========================");
-	prueba_simple();
+	pa2m_nuevo_grupo("\n======================== Creación de hash ========================");
+	creacion_de_hash();
 
 	return pa2m_mostrar_reporte();
 }
--- src/hash.c
+++ src/hash.c
@@ -1,13 +1,47 @@
 #include <string.h>
 #include <stdlib.h>
-
+#include <stdbool.h>
+#include <stdio.h>
 #include "hash.h"
 
 #define FACTOR_CARGA_MAXIMA 0.7
+#define CAPACIDAD_MIN 3
+
+typedef enum { VACIO, OCUPADO, ELIMINADO } estado;
+struct par {
+	char* clave;
+	void* valor;
+	int estado;
+};
+
+struct hash {
+	size_t capacidad;
+	struct par* pares;
+	size_t cantidad_elementos;
+};
+
 
 hash_t *hash_crear(size_t capacidad)
-{
-	return NULL;
+{    
+	hash_t* hash = malloc(sizeof(hash_t));
+	if (!hash) {
+		return NULL;
+	}
+
+	if (capacidad < CAPACIDAD_MIN) {
+		hash->capacidad = CAPACIDAD_MIN;
+	}
+
+	hash->capacidad = capacidad;
+	hash->cantidad_elementos = 0;
+
+	hash->pares = malloc(sizeof(struct par) * hash->capacidad);
+	if (!hash->pares) {
+		free(hash);
+		return NULL;
+	}	
+
+	return hash;
 }
 
 hash_t *hash_insertar(hash_t *hash, const char *clave, void *elemento,
@@ -33,15 +67,23 @@ bool hash_contiene(hash_t *hash, const char *clave)
 
 size_t hash_cantidad(hash_t *hash)
 {
-	return 0;
+	return hash->cantidad_elementos;
 }
+
+void hash_destruir_todo(hash_t *hash, void (*destructor)(void *)) {
+    if (!hash) {
+        return;
+    }
+	//FALTA HACER FREE DE MAS COSAS
+    free(hash->pares);
+    free(hash);
+}
+
+
 
 void hash_destruir(hash_t *hash)
 {
-}
-
-void hash_destruir_todo(hash_t *hash, void (*destructor)(void *))
-{
+	hash_destruir_todo(hash, NULL);
 }
 
 size_t hash_con_cada_clave(hash_t *hash,
